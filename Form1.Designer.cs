383bb8c7e048ac341b3373bcfb74413c4bc4da04
zhangac <zhangac@DESKTOP-JTLMTU6>
2023-06-01 20:29:12 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace MessageClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            textBox2 = new TextBox();
+            button3 = new Button();
+            textBox3 = new TextBox();
+            button2 = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(375, 45);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 5;
+            button1.Text = "连接服务";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += new System.EventHandler(button1_Click);
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(189, 47);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 30);
+            textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(58, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(116, 24);
+            label1.TabIndex = 3;
+            label1.Text = "服务器IP地址";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Location = new Point(44, 100);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(459, 591);
+            groupBox1.TabIndex = 6;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "聊天区";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(23, 374);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(420, 162);
+            textBox2.TabIndex = 8;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(331, 542);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 34);
+            button3.TabIndex = 9;
+            button3.Text = "发送";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += new System.EventHandler(button3_Click);
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(23, 29);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.Size = new Size(420, 299);
+            textBox3.TabIndex = 9;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(331, 334);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 7;
+            button2.Text = "清空";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 24F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(560, 740);
+            Controls.Add(groupBox1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private GroupBox groupBox1;
+        private TextBox textBox2;
+        private Button button3;
+        private TextBox textBox3;
+        private Button button2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,231 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Xml.Linq;
+
+namespace MessageClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ipadr = IPAddress.Loopback;
+        }
+
+        Socket clientSocket = null;
+        static Boolean isListen = true;
+        Thread thDataFromServer;
+        IPAddress ipadr;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SendMessage();
+        }
+        private void SendMessage()
+        {
+            if (String.IsNullOrWhiteSpace(textBox2.Text.Trim()))
+            {
+                MessageBox.Show("发送内容不能为空哦~");
+                return;
+            }
+            if (clientSocket != null && clientSocket.Connected)
+            {
+                Byte[] bytesSend = Encoding.UTF8.GetBytes(textBox2.Text + "$");
+                clientSocket.Send(bytesSend);
+                textBox2.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("未连接服务器或者服务器已停止，请联系管理员~");
+                return;
+            }
+        }
+
+
+        //每一个连接的客户端必须设置一个唯一的用户名，在服务器端是把用户名和套接字保存在Dictionary<userName,ClientSocket>中
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (clientSocket == null || !clientSocket.Connected)
+            {
+                try
+                {
+                    clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    //参考网址： https://msdn.microsoft.com/zh-cn/library/6aeby4wt.aspx
+                    // Socket.BeginConnect 方法 (String, Int32, AsyncCallback, Object)
+                    //开始一个对远程主机连接的异步请求
+                    /* string host,     远程主机名
+                     * int port,        远程主机的端口
+                     * AsyncCallback requestCallback,   一个 AsyncCallback 委托，它引用连接操作完成时要调用的方法，也是一个异步的操作
+                     * object state     一个用户定义对象，其中包含连接操作的相关信息。 当操作完成时，此对象会被传递给 requestCallback 委托
+                     */
+                    //如果txtIP里面有值，就选择填入的IP作为服务器IP，不填的话就默认是本机的
+                    if (!String.IsNullOrWhiteSpace(textBox1.Text.ToString().Trim()))
+                    {
+                        try
+                        {
+                            ipadr = IPAddress.Parse(textBox1.Text.ToString().Trim());
+                        }
+                        catch
+                        {
+                            MessageBox.Show("请输入正确的IP后重试");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        ipadr = IPAddress.Loopback;
+                    }
+                    //IPAddress ipadr = IPAddress.Parse("192.168.1.100");
+                    clientSocket.BeginConnect(ipadr, 8080, (args) =>
+                    {
+                        if (args.IsCompleted)   //判断该异步操作是否执行完毕
+                        {
+                            Byte[] bytesSend = new Byte[4096];
+                            textBox1.BeginInvoke(new Action(() =>
+                            {
+                                if (clientSocket != null && clientSocket.Connected)
+                                {
+                                    textBox1.Enabled = false;
+                                }
+                            }));
+                        }
+                    }, null);
+                }
+                catch (SocketException ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            else
+            {
+                MessageBox.Show("你已经连接上服务器了");
+            }
+        }
+
+        private void ShowMsg(String msg)
+        {
+            textBox3.BeginInvoke(new Action(() =>
+            {
+                textBox3.Text += Environment.NewLine + msg;    // 在 Windows 环境中，C# 语言 Environment.NewLine == "\r\n" 结果为 true
+                //txtReceiveMsg.ScrollToCaret();
+            }));
+        }
+
+        //获取服务器端的消息
+        private void DataFromServer()
+        {
+            ShowMsg("Connected to the Chat Server...");
+            isListen = true;
+            try
+            {
+                while (isListen)
+                {
+                    Byte[] bytesFrom = new Byte[4096];
+                    Int32 len = clientSocket.Receive(bytesFrom);
+
+                    String dataFromClient = Encoding.UTF8.GetString(bytesFrom, 0, len);
+                    if (!String.IsNullOrWhiteSpace(dataFromClient))
+                    {
+                        //如果收到服务器已经关闭的消息，那么就把客户端接口关了，免得出错，并在客户端界面上显示出来
+                        if (dataFromClient.ToString().Length >= 17 && dataFromClient.ToString().Substring(0, 17).Equals("Server has closed"))
+                        {
+                            clientSocket.Close();
+                            clientSocket = null;
+
+                            textBox3.BeginInvoke(new Action(() =>
+                            {
+                                textBox3.Text += Environment.NewLine + "服务器已关闭";
+                            }));
+
+                            textBox1.BeginInvoke(new Action(() =>
+                            {
+
+                                textBox1.Enabled = true;
+
+                            }));
+
+                            thDataFromServer.Abort();   //这一句必须放在最后，不然这个进程都关了后面的就不会执行了
+
+                            return;
+                        }
+
+
+                        if (dataFromClient.StartsWith("#") && dataFromClient.EndsWith("#"))
+                        {
+                            String userName = dataFromClient.Substring(1, dataFromClient.Length - 2);
+                            this.BeginInvoke(new Action(() =>
+                            {
+                                MessageBox.Show("用户名：[" + userName + "]已经存在，请尝试其他用户名并重试");
+
+                            }));
+                            isListen = false;
+
+                            textBox1.BeginInvoke(new Action(() =>
+                            {
+
+                                textBox1.Enabled = true;
+
+                            }));
+
+                        }
+                        else
+                        {
+                            //txtName.Enabled = false;    //当用户名唯一时才禁止再次输入用户名
+                            ShowMsg(dataFromClient);
+                        }
+                    }
+                }
+            }
+            catch (SocketException ex)
+            {
+                isListen = false;
+                if (clientSocket != null && clientSocket.Connected)
+                {
+                    //没有在客户端关闭连接，而是给服务器发送一个消息，在服务器端关闭连接
+                    //这样可以将异常的处理放到服务器。客户端关闭会让客户端和服务器都抛异常
+                    clientSocket.Send(Encoding.UTF8.GetBytes("$"));
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            textBox2.Focus();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (clientSocket != null && clientSocket.Connected)
+            {
+                clientSocket.Send(Encoding.UTF8.GetBytes("$"));
+            }
+        }
+
+        private void btnBreak_Click(object sender, EventArgs e)
+        {
+            if (clientSocket != null && clientSocket.Connected)
+            {
+                thDataFromServer.Abort();
+                clientSocket.Send(Encoding.UTF8.GetBytes("$"));
+
+                clientSocket.Close();
+                clientSocket = null;
+
+                textBox3.BeginInvoke(new Action(() =>
+                {
+                    textBox3.Text += Environment.NewLine + "已断开与服务器的连接";
+                }));
+
+                textBox1.BeginInvoke(new Action(() =>
+                {
+
+                    textBox1.Enabled = true;
+
+                }));
+            }
+        }
+    }
+}
